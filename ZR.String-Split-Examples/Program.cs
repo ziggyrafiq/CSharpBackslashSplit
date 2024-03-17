1bd739e120cdea8a03c29916e51992f41a2a3afa
Ziggy Rafiq <ziggyrafiq@hotmail.com>
2024-03-17 15:48:01 +0000

    C# Split String Where There Is the Backslash
    C# Split String Where There Is the Backslash

---
+++ ZR.String-Split-Examples/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string input = "C:\\Users\\Ziggy\\Documents\\";
+//string[] result = input.Split('\\');
+string[] result = input.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+
+if (result.Length > 0)
+{
+    foreach (string output in result)
+    {
+        Console.WriteLine(output);
+    }
+}
+
+Console.ReadLine();
+
+
+
